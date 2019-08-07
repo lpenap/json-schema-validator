684f9b99ed4ec9f3527978d1f2212c04fdd77ce8
Luis Augusto Peña Pereira <lpenap@gmail.com>
2019-08-07 16:03:26 -0300

    Add code cleanup

--- src/main/java/com/penapereira/json/validator/util/Util.java
+++ src/main/java/com/penapereira/json/validator/util/Util.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.util.Map;
 import java.util.Set;
@@ -13,9 +15,31 @@ import javax.net.ssl.HttpsURLConnection;
 public class Util {
 
 	public String requestJson(String endpoint, String method, Map<String, String> headers) throws IOException {
+		HttpURLConnection conn = createConnection(endpoint, method, headers);
+		conn.connect();
+		if (conn.getResponseCode() != 200) {
+			throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+		}
+		StringBuffer buffer = readStringFromConnection(conn);
+		conn.disconnect();
+		return buffer.toString();
+	}
+
+	private StringBuffer readStringFromConnection(HttpURLConnection conn) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+		StringBuffer buffer = new StringBuffer();
+		String output;
+		while ((output = br.readLine()) != null) {
+			buffer.append(output);
+		}
+		return buffer;
+	}
+
+	private HttpURLConnection createConnection(String endpoint, String method, Map<String, String> headers)
+			throws MalformedURLException, IOException, ProtocolException {
 		URL url = new URL(endpoint);
-		HttpURLConnection conn = null;		
-		
+		HttpURLConnection conn = null;
+
 		if (url.getProtocol().equalsIgnoreCase("https")) {
 			conn = (HttpsURLConnection) url.openConnection();
 		} else {
@@ -24,40 +48,22 @@ public class Util {
 
 		conn.setRequestMethod(method);
 		conn.setRequestProperty("Accept", "application/json");
+
 		// adding headers
-		Set<String> keys = headers.keySet();
-		for (String key : keys) {
+		for (String key : headers.keySet()) {
 			conn.setRequestProperty(key, headers.get(key));
 		}
-		conn.connect();
-
-		if (conn.getResponseCode() != 200) {
-			throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
-		}
-
-		BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-		StringBuffer buffer = new StringBuffer();
-		String output;
-		while ((output = br.readLine()) != null) {
-			buffer.append(output);
-		}
-
-		conn.disconnect();
-
-		return buffer.toString();
+		return conn;
 	}
 
 	public String readFile(String path) {
 		StringBuffer buffer = new StringBuffer();
 		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(path)));
-
 			String sCurrentLine;
-
 			while ((sCurrentLine = br.readLine()) != null) {
 				buffer.append(sCurrentLine);
 			}
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
